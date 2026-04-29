aae53fcafb588aea2b8e815baa3a2a8fd217505c
jhs512 <jangka512@gmail.com>
2026-04-29 14:27:26 +0900

    WiseSayingControllerTest.t1 green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -3,15 +3,33 @@ package com.back;
 import java.util.Scanner;
 
 public class App {
-    public App(Scanner scanner) {
+    private final Scanner scanner;
 
+    public App(Scanner scanner) {
+        this.scanner = scanner;
     }
 
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        System.out.print("명령) ");
+        while (true) {
+            System.out.print("명령) ");
 
-        System.out.println("프로그램이 종료되었습니다.");
+            String cmd = scanner.nextLine().trim();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String content = scanner.nextLine().trim();
+
+                    System.out.print("작가 : ");
+                    String author = scanner.nextLine().trim();
+                }
+                case "종료" -> {
+                    System.out.println("프로그램이 종료되었습니다.");
+                    return;
+                }
+            }
+        }
     }
 }
--- src/test/java/com/back/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/back/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -10,7 +10,11 @@ public class WiseSayingControllerTest {
     @Test
     @DisplayName("등록")
     void t1() {
-        final String out = AppTestRunner.run("등록");
+        final String out = AppTestRunner.run("""
+                등록
+                나의 죽음을 적들에게 알리지 말라.
+                이순신
+                """);
 
         assertThat(out)
                 .contains("명언 : ")
