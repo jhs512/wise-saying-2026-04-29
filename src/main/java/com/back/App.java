b41da6bd4a758b6f08c62e70cc97d693f1751606
jhs512 <jangka512@gmail.com>
2026-04-29 14:21:00 +0900

    AppTest.t2 green

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -9,5 +9,9 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        System.out.print("명령) ");
+
+        System.out.println("프로그램이 종료되었습니다.");
     }
 }
