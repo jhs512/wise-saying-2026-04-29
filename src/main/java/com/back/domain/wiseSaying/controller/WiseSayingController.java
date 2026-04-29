73c5994090195951b16b4a9a728b5f8eb307039a
jhs512 <jangka512@gmail.com>
2026-04-29 14:35:21 +0900

    WiseSayingControllerTest.t3 green

--- src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class WiseSayingController {
     private final Scanner scanner;
+    private int lastId;
 
     public WiseSayingController(Scanner scanner) {
         this.scanner = scanner;
+        lastId = 0;
     }
 
     public void actionWrite() {
@@ -16,6 +18,8 @@ public class WiseSayingController {
         System.out.print("작가 : ");
         String author = scanner.nextLine().trim();
 
-        System.out.println("1번 명언이 등록되었습니다.");
+        int id = ++lastId;
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", id);
     }
 }
